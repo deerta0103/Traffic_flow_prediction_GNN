184fdfbb44051c4ae7faa11fff34d189d8d44dda
MuHongfan <amber1216@live.cn>
2021-03-10 09:05:05 -0500

    Load and get adjacent matrix

--- data_processing.py
+++ data_processing.py
@@ -3,58 +3,6 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 
-def get_adjacent_matrix(distance_file: str, num_nodes: int, id_file: str = None, graph_type="connect") -> np.array:
-    """
-    :param distance_file: str, 用于保存节点之间距离的文件
-    :param num_nodes: int, number of nodes in the graph
-    :param id_file: str, 保存节点之间绝对顺序的文件.
-    :param graph_type: str, ["connect", "distance"] ，可以选择是否使用距离作为边的权重
-    :return:  邻接矩阵A ，np.array(N, N)
-    """
-    A = np.zeros([int(num_nodes), int(num_nodes)])
-
-    if id_file:
-        with open(id_file, "r") as f_id:
-            node_id_dict = {int(node_id): idx for idx, node_id in enumerate(f_id.read().strip().split("\n"))}
-
-            with open(distance_file, "r") as f_d:
-                f_d.readline()
-                reader = csv.reader(f_d)
-                for item in reader:
-                    if len(item) != 3:
-                        continue
-                    i, j, distance = int(item[0]), int(item[1]), float(item[2])
-                    if graph_type == "connect":
-                        A[node_id_dict[i], node_id_dict[j]] = 1.
-                        A[node_id_dict[j], node_id_dict[i]] = 1.
-                    elif graph_type == "distance":
-                        A[node_id_dict[i], node_id_dict[j]] = 1. / distance
-                        A[node_id_dict[j], node_id_dict[i]] = 1. / distance
-                    else:
-                        raise ValueError("graph type is not correct (connect or distance)")
-        return A
-
-    with open(distance_file, "r") as f_d:
-        f_d.readline()
-        reader = csv.reader(f_d)
-        for item in reader:
-            if len(item) != 3:
-                continue
-            i, j, distance = int(item[0]), int(item[1]), float(item[2])
-
-            if graph_type == "connect":
-                A[i, j], A[j, i] = 1., 1.
-            elif graph_type == "distance":
-                A[i, j] = 1. / distance
-                A[j, i] = 1. / distance
-            else:
-                raise ValueError("graph type is not correct (connect or distance)")
-
-    return A
-
-
-
-
 class LoadData(Dataset):
     def __init__(self, data_path, num_nodes, divide_days, time_interval, history_length, train_mode):
         """
@@ -196,6 +144,55 @@ class LoadData(Dataset):
     @staticmethod
     def to_tensor(data):
         return torch.tensor(data, dtype=torch.float)
+
+def get_adjacent_matrix(distance_file: str, num_nodes: int, id_file: str = None, graph_type="connect") -> np.array:
+    """
+    :param distance_file: str, 用于保存节点之间距离的文件
+    :param num_nodes: int, number of nodes in the graph
+    :param id_file: str, 保存节点之间绝对顺序的文件.
+    :param graph_type: str, ["connect", "distance"] ，可以选择是否使用距离作为边的权重
+    :return:  邻接矩阵A ，np.array(N, N)
+    """
+    A = np.zeros([int(num_nodes), int(num_nodes)])
+
+    if id_file:
+        with open(id_file, "r") as f_id:
+            node_id_dict = {int(node_id): idx for idx, node_id in enumerate(f_id.read().strip().split("\n"))}
+
+            with open(distance_file, "r") as f_d:
+                f_d.readline()
+                reader = csv.reader(f_d)
+                for item in reader:
+                    if len(item) != 3:
+                        continue
+                    i, j, distance = int(item[0]), int(item[1]), float(item[2])
+                    if graph_type == "connect":
+                        A[node_id_dict[i], node_id_dict[j]] = 1.
+                        A[node_id_dict[j], node_id_dict[i]] = 1.
+                    elif graph_type == "distance":
+                        A[node_id_dict[i], node_id_dict[j]] = 1. / distance
+                        A[node_id_dict[j], node_id_dict[i]] = 1. / distance
+                    else:
+                        raise ValueError("graph type is not correct (connect or distance)")
+        return A
+
+    with open(distance_file, "r") as f_d:
+        f_d.readline()
+        reader = csv.reader(f_d)
+        for item in reader:
+            if len(item) != 3:
+                continue
+            i, j, distance = int(item[0]), int(item[1]), float(item[2])
+
+            if graph_type == "connect":
+                A[i, j], A[j, i] = 1., 1.
+            elif graph_type == "distance":
+                A[i, j] = 1. / distance
+                A[j, i] = 1. / distance
+            else:
+                raise ValueError("graph type is not correct (connect or distance)")
+
+    return A
 
 
 if __name__ == '__main__':
